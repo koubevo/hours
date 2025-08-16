8fcdb66325fc8e5a263262150b43fa0f5da514bb
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2025-08-16 13:18:55 +0200

    fix tailwind issue

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
+        './vendor/livewire/flux/**/*.blade.php',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
